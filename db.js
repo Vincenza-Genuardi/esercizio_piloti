bf7984a36ec86e7dd17d5a0483b139d3ae8cbf70
Vincenza-Genuardi <92849502+Vincenza-Genuardi@users.noreply.github.com>
2023-03-17 11:10:20 +0100

    commit file json

---
+++ db.js
@@ -0,0 +1,32 @@
+[
+    {
+        "numero": 1,
+        "nome": "Verstappen"
+        "macchina": "Red Bull"
+    },
+    {
+        "numero": 11,
+        "nome": "Perez"
+        "macchina": "Red Bull"
+    },
+    {
+        "numero": 16,
+        "nome": "Leclerc"
+        "macchina": "Ferrari"
+    },
+    {
+        "numero": 55,
+        "nome": "Sainz"
+        "macchina": "Ferrari"
+    },
+    {
+        "numero": 44,
+        "nome": "Hamilton"
+        "macchina": "Mercedes"
+    },
+    {
+        "numero": 63,
+        "nome": "Russel"
+        "macchina": "Mercedes"
+    }
+]
